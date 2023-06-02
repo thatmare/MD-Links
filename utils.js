458683854312a5a011c202db5e7b9855434b5046
Marissa Vargas <marissa.lorena@hotmail.com>
2023-06-02 14:55:49 -0600

    fx readingFile en utils.js que retorna promesa, implementacion correcta para leer el contenido de archivo .md

--- utils.js
+++ utils.js
@@ -12,15 +12,48 @@
 
 // console.log(fs.existsSync(pathInput))
 
-const fs = require('fs')  
-const file = process.argv[2]  
-  
-fs.readFile(file, 'utf8', (err, data) => {
- const lines = data.split('\n').length - 1;
- console.log(lines);
-})
+// const fs = require('fs')  
+// // const folder = process.argv[2]  
 
-// module.exports = {
-//     isPathAbsolute,
-//     pathToAbsolute,
+// // fs.readdir(folder, function(err, data) { // consolea los archivos dentro de ./carpetaejemplo
+// //     data.forEach(i => console.log(i));
+// // })
+
+// // const file = process.argv[2];
+// // const path = require('node:path')
+
+// const readingFile = (f) => {
+//     fs.readFile(f, 'utf8', (err, data) => {
+//         if(err) throw error;
+//         return data;
+//     })
 // }
+
+// fs.readFile(file, 'utf8', (err, data) => { // consolea las lineas de sample.md a la misma altura de la carpeta
+//     console.log(data);
+// })
+
+//console.log(path.extname(file)) // consolea la extension del archivo dado sample.md
+  
+// fs.readFile(file, 'utf8', (err, data) => {
+//  const lines = data.split('\n').length - 1;
+//  console.log(lines);
+// })
+
+const fs = require('fs');
+
+const readingFile = (f) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(f, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+module.exports = {
+  readingFile,
+};
