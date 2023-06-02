8bf10a3c73747150d47d90c12cc65f261494ea40
Marissa Vargas <marissa.lorena@hotmail.com>
2023-06-02 11:05:10 -0600

    test

--- utils.js
+++ utils.js
@@ -1,20 +1,26 @@
-// fxs para fx mdlinks
+// const fs = require('fs'); // file system
+// const path = require('node:path');
+// const pathInput = process.argv[2];
 
-const fs = require('fs'); // file system
-const path = require('node:path');
-const pathInput = process.argv[2];
+// const isPathAbsolute = (pathInput) => { // output es un booleano: true si es absoluta, false si es relativa
+//     return path.isAbsolute(pathInput);
+// };
 
-const isPathAbsolute = (pathInput) => { // output es un booleano: true si es absoluta, false si es relativa
-    return path.isAbsolute(pathInput);
-};
+// const pathToAbsolute = (pathInput) => { // input es ruta relativa, output es la ruta absoluta en string
+//     return path.resolve(pathInput);
+// };
 
-const pathToAbsolute = (pathInput) => { // input es ruta relativa, output es la ruta absoluta en string
-    return path.resolve(pathInput);
-};
+// console.log(fs.existsSync(pathInput))
 
-console.log(fs.existsSync(pathInput))
+const fs = require('fs')  
+const file = process.argv[2]  
+  
+fs.readFile(file, 'utf8', (err, data) => {
+ const lines = data.split('\n').length - 1;
+ console.log(lines);
+})
 
-module.exports = {
-    isPathAbsolute,
-    pathToAbsolute,
-}
+// module.exports = {
+//     isPathAbsolute,
+//     pathToAbsolute,
+// }
