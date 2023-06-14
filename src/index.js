79ce2079a4f58fd8efebff3c0dadc6d4855a1cd6
Marissa Vargas <marissa.lorena@hotmail.com>
2023-06-13 18:14:01 -0600

    refactorizando index.js

--- src/index.js
+++ src/index.js
@@ -1,78 +1,166 @@
 const { resolvePath, doesPathExist, filterDirectorySync, readingFile, filterLinks, httpRequest, isItFile } = require('./utils.js');
 const pathInput = process.argv[2]
 
-const mdlinks = (pathInput, options = { validate }) => {
+const linksToAnalyze = (pathInput) => {
   return new Promise((resolve, reject) => {
-    const pathResolved = resolvePath(pathInput);
-    doesPathExist(pathResolved)
-      .then((existingPath) => {
-        return isItFile(existingPath)
-          .then((isFile) => {
-            if (isFile) {
-              if(options.validate === false) {
-                return readingFile(existingPath)
-                .then((content) => {
-                  const links = filterLinks(existingPath, content);
-                  resolve(links);
-                })
-              } else {
-                return readingFile(existingPath)
-                .then((content) => {
-                  const links = filterLinks(existingPath, content);
-                  resolve(httpRequest(existingPath, links));
-                })
-              }
-            } else {
-              if(options.validate === false) {
-                const data = filterDirectorySync(existingPath);
-                const allPromises = data.map(file => {
-                  return readingFile(file)
-                    .then(content => {
-                      const links = filterLinks(file, content);
-                      return links;
-                    });
-                });
-
-                Promise.all(allPromises)
-                  .then(results => {
-                    const allLinks = results.flat();
-                    resolve(allLinks);
-                  });
-              } else {
-                const data = filterDirectorySync(existingPath);
-                const allPromises = data.map(file => {
-                  return readingFile(file)
-                    .then(content => {
-                      const links = filterLinks(file, content);
-                      return httpRequest(file, links)
-                    });
-                });
-
-                Promise.all(allPromises)
-                  .then(results => {
-                    const allLinks = results.flat();
-                    resolve(allLinks);
-                  });
-              }
-            } 
+  const pathResolved = resolvePath(pathInput);
+  doesPathExist(pathResolved).then(existingPath => {
+    return isItFile(existingPath).then(isFile => {
+      if(isFile) {
+        return readingFile(existingPath).then(content => {
+          const links = filterLinks(existingPath, content)
+          resolve(links)
+        });
+      } else {
+        const data = filterDirectorySync(existingPath);
+        const allLinksFromDir = data.map(file => {
+          return readingFile(file).then(content => {
+            const links = filterLinks(existingPath, content);
+            return links;
           });
-      })
-      .catch((err) => {
-        reject(err); // rechazar la promesa de mdlinks
-      });
+        });
+        Promise.all(allLinksFromDir).then(results => {
+          const allLinks = results.flat();
+          resolve(allLinks);
+        });
+      };
+    });
+  })
+    .catch(err => {
+      reject(err)
+    })  
   });
 };
 
-const promesa = mdlinks(pathInput, {validate: true})
-console.log(promesa, 'aqui promesa')
-  promesa
-  .then((result) => {
-    console.log(result, 'then del resultado');
+const mdLinks = (options = { validate }) => {
+  return new Promise((resolve, reject) => {
+    linksToAnalyze(pathInput).then(links => {
+      if(options.validate === false || !options.validate) {
+        resolve(links)
+      } else {
+        resolve(httpRequest(pathInput, links))
+      }
+    })
+    .catch(err => {
+      reject(err)
+    })
   })
-  .catch((err) => {
-    console.error(err, 'aqui catch del error');
+}
+
+// linksToAnalyze('C://Users//maris//Desktop//coding//Laboratoria//md-links//md-links//carpetaejemplo//tale.md')
+//   .then(result => {
+//   console.log(result)
+// }).catch(err => {
+//   console.error(err)
+// })
+
+// const promesa = mdLinks({validate: false});
+// console.log(promesa, 'aqui promesa de links')
+// promesa.then(result => {
+//   console.log(result, 'aqui then del resultado')
+// }).catch(err => {
+//   console.error(err, 'aqui catch de error')
+// })
+
+linksToAnalyze(pathInput)
+  .then(result => {
+    return result;
+  })
+  .catch(err => {
+    console.error(err);
   });
 
-module.exports = {
-  mdlinks,
-}
+const promesa = mdLinks({ validate: true });
+console.log(promesa, 'aqui promesa de links');
+promesa
+  .then(result => {
+    console.log(result, 'aqui then del resultado');
+  })
+  .catch(err => {
+    console.error(err, 'aqui catch de error');
+  });
+
+
+// const promesa = linksToAnalyze(pathInput);
+// console.log(promesa, 'aqui promesa de links')
+// promesa.then(result => {
+//   console.log(result, 'aqui then del resultado')
+// }).catch(err => {
+//   console.error(err, 'aqui catch de error')
+// })
+
+// const mdlinks = (pathInput, options = { validate }) => {
+//   return new Promise((resolve, reject) => {
+//     const pathResolved = resolvePath(pathInput);
+//     doesPathExist(pathResolved)
+//       .then((existingPath) => {
+//         return isItFile(existingPath)
+//           .then((isFile) => {
+//             if (isFile) {
+//               if(options.validate === false) {
+//                 return readingFile(existingPath)
+//                 .then((content) => {
+//                   const links = filterLinks(existingPath, content);
+//                   resolve(links);
+//                 })
+//               } else {
+//                 return readingFile(existingPath)
+//                 .then((content) => {
+//                   const links = filterLinks(existingPath, content);
+//                   resolve(httpRequest(existingPath, links));
+//                 })
+//               }
+//             } else {
+//               if(options.validate === false) {
+//                 const data = filterDirectorySync(existingPath);
+//                 const allPromises = data.map(file => {
+//                   return readingFile(file)
+//                     .then(content => {
+//                       const links = filterLinks(file, content);
+//                       return links;
+//                     });
+//                 });
+
+//                 Promise.all(allPromises)
+//                   .then(results => {
+//                     const allLinks = results.flat();
+//                     resolve(allLinks);
+//                   });
+//               } else {
+//                 const data = filterDirectorySync(existingPath);
+//                 const allPromises = data.map(file => {
+//                   return readingFile(file)
+//                     .then(content => {
+//                       const links = filterLinks(file, content);
+//                       return httpRequest(file, links)
+//                     });
+//                 });
+
+//                 Promise.all(allPromises)
+//                   .then(results => {
+//                     const allLinks = results.flat();
+//                     resolve(allLinks);
+//                   });
+//               }
+//             } 
+//           });
+//       })
+//       .catch((err) => {
+//         reject(err); // rechazar la promesa de mdlinks
+//       });
+//   });
+// };
+
+// const promesa = mdlinks(pathInput, {validate: true})
+// console.log(promesa, 'aqui promesa')
+//   promesa
+//   .then((result) => {
+//     console.log(result, 'then del resultado');
+//   })
+//   .catch((err) => {
+//     console.error(err, 'aqui catch del error');
+//   });
+
+// module.exports = {
+//   mdlinks,
+// }
