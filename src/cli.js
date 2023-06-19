65ccf7d73d7fb25f51284d48d7b327cd5ed9fcfe
Marissa Vargas <marissa.lorena@hotmail.com>
2023-06-19 17:47:36 -0600

    cfonts and --help option added

--- src/cli.js
+++ src/cli.js
@@ -2,19 +2,29 @@
 
 const { mdLinks } = require('./index.js');
 const { countUniqueLinks, countBrokenLinks } = require('./utils.js')
+const cfonts = require('cfonts');
 const pathInput = process.argv[2];
 const validateOption = process.argv.includes('--validate');
 const statsOption = process.argv.includes('--stats');
+const helpOption = process.argv.includes('--help');
 const colors = {
     blue: '\x1b[38;5;38m',
     teal: '\x1b[38;5;43m',
     pink: '\x1b[38;5;170m',
     yellow: '\x1b[38;5;220m',
     red: '\x1b[38;5;160m',
+    highlighter: '\x1b[48;5;78m',
     reset: '\x1b[0m'// Reset to default color
   };
 
-if(!validateOption && !statsOption) {
+if(helpOption) {
+  cfonts.say('md-links', {
+    font: 'tiny',
+    gradient: ['#e9ff49', '#f186ff'],
+    transitionGradient: true,
+  })
+  console.log(`md-links is a command-line program that identifies links in Markdown files, just as validating their HTTP status and counting them. \n \n To use md-links you must add the following information: \n \n 1. ${colors.highlighter}md-links <path>:${colors.reset} identifies the links in all Markdown files found. \n \n 2. ${colors.highlighter}md-links <path> --validate:${colors.reset} validates the HTTP status of all the links. \n \n 3. ${colors.highlighter}md-links <path> --stats:${colors.reset} counts the total and unique amount of links. \n \n 4. ${colors.highlighter}md-links <path> --validate --stats:${colors.reset} besides the total and unique amount of links, it also counts the broken links.`)
+} else if(!validateOption && !statsOption) {
   mdLinks(pathInput, { validate: false })
     .then(linksArray => {
       if(linksArray.length === 0) {
